0f5d4c2e9c31f6140a0a3beb96f47a4cfbb41015
nastya <meshcheryakova-nastya@internet.ru>
2026-03-20 13:03:58 +0300

    Day 7: Added CRUD operations with edit cancel

--- app/src/main/java/com/example/myapplication/dto/Post.kt
+++ app/src/main/java/com/example/myapplication/dto/Post.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.dto
 data class Post(
     val id: Long,
     val author: String,
+    val authorId: Long = 0,  // добавим для идентификации автора
     val content: String,
     val published: String,
     val likedByMe: Boolean = false,
@@ -10,3 +11,5 @@ data class Post(
     val shares: Int = 0,
     val views: Int = 0
 )
+
+
--- app/src/main/java/com/example/myapplication/repository/PostRepository.kt
+++ app/src/main/java/com/example/myapplication/repository/PostRepository.kt
@@ -8,5 +8,8 @@ interface PostRepository {
     fun likeById(id: Long)
     fun shareById(id: Long)
     fun increaseViews(id: Long)
+    fun save(post: Post)        // для создания и обновления
+    fun removeById(id: Long)     // для удаления
 }
+
 
